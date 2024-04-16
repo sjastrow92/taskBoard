6e231f22c8edb1d787aeae699693cdc82169dfc5
sjastrow <skjastrow@gmail.com>
2024-04-15 22:43:13 -0400

    added functions in js to handle deleting a task card. created a function to render cards colors based on due date. added screenshot images to ReadMe file. Completed ReadMe file.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # taskBoard
-I am creating a task baord that will allow me to add tasks and track its progress through multiple stages that are color coded.
+
+I am creating a task baord that will allow me to add tasks and track its progress through multiple stages that are color coded. it is complete when tasks can be created and dragged from column to column baswd on status, and be color coded according to due date.
+
+# Installation
+
+- select the "add task" button to pull uip the task form.
+- click, drag, and drop tasks into the appropriate column based on their current status.
+- see screen shots below.
+
+- screenshot of task card form
+  ![form screen shot](https://github.com/sjastrow92/taskBoard/assets/164063940/1a6667d4-33a6-475a-8a03-55fc104e9373)
+
+- screenshot of due date labels that are color coded
+  ![task label screenshot](https://github.com/sjastrow92/taskBoard/assets/164063940/6bae45c5-31c5-47b1-8982-c6adda43b410)
+
+- screenshot of drag and drop
+  ![drag and drop screenshot](https://github.com/sjastrow92/taskBoard/assets/164063940/0c8ac7a6-a01f-4661-94c5-5101fd6f6aa1)
+
+# Credits
+
+- starter code was provided by instrutor.
+- in folder 05, I used the completed instructor version of the student mini project (file 28) as a reference for this project.
+- w3 schools on how to creat a form in html was used.
+  https://www.w3schools.com/html/html_forms.asp
+- tutors John and Mansi were able to asisst me in creating the create task card function and the delete app function. Mansi was also able to asisst me with deleting the card task.
+- chat gpt was used to clena up my html form.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
-// const addTaskButton = $(".button-success");
-// const modal = $(".modal");
-// todo: create an onclick funciton to display modal
 
-// Todo: create a function to generate a unique task id
+// create an onclick funciton to display modal
+
+// created a function to generate a unique task id
 function generateTaskId() {
   if (nextId === null) {
     nextId = 1;
@@ -17,28 +16,42 @@ function generateTaskId() {
   return nextId;
 }
 
-// Todo: create a function to create a task card
+// created a function to create a task card
 function createTaskCard(task) {
-  // TODO: Create a new card element and add the classes `card`, `project-card`, `draggable`, and `my-3`. Also add a `data-project-id` attribute and set it to the project id.
+  // new card element and add the classes `card`, `project-card`, `draggable`, and `my-3`. Also add a `data-project-id` attribute and set it to the project id.
   let card = $("<div>")
     .addClass("card project-card draggable")
     .attr("data-task-id", task.id);
-  // TODO: Create a new card header element and add the classes `card-header` and `h4`. Also set the text of the card header to the project name.
-  let cardHeader = $("<div>").addClass("card header h4").text(task.title);
-  // TODO: Create a new card body element and add the class `card-body`.
+  // new card header element and add the classes `card-header` and `h4`. Also set the text of the card header to the project name.
+  let cardHeader = $("<div>").addClass("card-header h4").text(task.title);
+  // new card body element and add the class `card-body`.
   let cardBody = $("<div>").addClass("card-body");
-  // TODO: Create a new paragraph element and add the class `card-text`. Also set the text of the paragraph to the project type.
+  // new paragraph element and add the class `card-text`. Also set the text of the paragraph to the project type.
   let cardParagraph = $("<div>").addClass("card-text").text(task.description);
-  // TODO: Create a new paragraph element and add the class `card-text`. Also set the text of the paragraph to the project due date.
+  // paragraph element and add the class `card-text`. Also set the text of the paragraph to the project due date.
   let cardDuedate = $("<div>")
     .addClass("card-text")
     //set up card background color based on the due date. Use dayJs to compare due date with today's date
     .text(task.dueDate);
-  // TODO: Create a new button element and add the classes `btn`, `btn-danger`, and `delete`. Also set the text of the button to "Delete" and add a `data-project-id` attribute and set it to the project id.
+  // new button element and add the classes `btn`, `btn-danger`, and `delete`. Also set the text of the button to "Delete" and add a `data-project-id` attribute and set it to the project id.
   let cardButton = $("<div>")
     .addClass("btn btn-danger delete")
     .text("Delete")
     .attr("data-task-id", task.id);
+  // making sure the delete button works
+  cardButton.on("click", handleDeleteTask);
+
+  if (task.dueDate && task.status != "done") {
+    const today = dayjs();
+    const taskDate = dayjs(task.dueDate, "DD/MM/YYYY");
+    console.log(today);
+    // setting color coding for status
+    if (today.isSame(taskDate, "day")) {
+      card.addClass("bg-warning text-white");
+    } else if (today.isAfter(taskDate)) {
+      card.addClass("bg-danger text-white");
+    }
+  }
 
   //create card HTML elements $("<div>")
   card.append(cardHeader, cardBody, cardParagraph, cardDuedate, cardButton);
@@ -46,7 +59,7 @@ function createTaskCard(task) {
   //return card
 }
 
-// Todo: create a function to render the task list and make cards draggable
+// creating a function to render the task list and make cards draggable
 function renderTaskList() {
   $(function () {
     $(".draggable").draggable({ zIndex: 50 });
@@ -70,7 +83,7 @@ function renderTaskList() {
   }
 }
 
-// Todo: create a function to handle adding a new task
+// created a function to handle adding a new task
 function handleAddTask(event) {
   event.preventDefault();
 
@@ -94,8 +107,15 @@ function handleAddTask(event) {
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event) {
-  const taskId = $(this).attr("#task");
-  const tasks = renderTaskList();
+  event.preventDefault();
+  // assures the string is now an integer
+  const taskId = parseInt($(this).attr("data-task-id"));
+  console.log(taskId);
+
+  taskList = taskList.filter((task) => task.id !== taskId);
+  console.log(taskList);
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+  renderTaskList();
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
